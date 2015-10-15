f2b92f9f30332f154d9ffefaeaf8d2ad01dfe25d
Tam Pham <tampham47@live.com>
2015-10-15 21:47:44 +0700

    add core of rest ful for user & task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,61 +1,69 @@
+// @capheshift.github.io 2015
+// @author
+
+var _ = require('lodash');
 var mongoose = require('mongoose');
 var Tasks = mongoose.model('Tasks');
 var Config = require('../config/config');
 var Utilities = require('../config/utilities');
+var RestApi = require('../cores/restful');
 
-exports.getTaskById = function(req, res) {
-	Tasks.findById(req.params.taskId, function(err, task) {
-		if (err) {
-			return res.jsonp(Utilities.response(false, {}, 'Do not get Task by taskId', 404));
-		} else {
-			return res.jsonp(Utilities.response(true, task));
-		}
-	});
-};
+module.exports = exports = _.assign(RestApi(Tasks), {
 
-exports.getTaskByUserId = function(req, res) {
-	Tasks.find({
-		_userId: req.user._id.toString()
-	}).populate('_id').exec(function(err, tasks) {
-		if (err) {
-			return res.jsonp(Utilities.response(false, {}, 'Do not get tasks by userId'));
-		} else {
-			res.jsonp(Utilities.response(true, tasks));
-		}
-	});
-};
-
-exports.createTask = function(req, res) {
-	console.log(req.body);
-	var task = new Tasks({
-		'_userId': req.user._id.toString(),
-		'isCompleted': req.body.isCompleted,
-		'date': req.body.date,
-		'content': req.body.content,
-		'estimation': req.body.estimation
-	});
-	console.log(task);
-	task.save(function(err, task) {
-		if (err) {
-			return res.jsonp(Utilities.response(false, {}, 'Do not save Task to Database', 404));
-		} else {
-			return res.jsonp(Utilities.response(true, task));
-		}
-	});
-};
-
-exports.updateTask = function(req, res) {
-
-};
-
-exports.deleteTask = function(req, res) {
-	Tasks.findByIdAndRemove(
-		req.user._id.toString(),
-		function(err) {
+	getTaskById: function(req, res) {
+		Tasks.findById(req.params.taskId, function(err, task) {
 			if (err) {
-				return res.jsonp(Utilities.response(false, {}, 'Do not delete Task ', 404));
+				return res.jsonp(Utilities.response(false, {}, 'Do not get Task by taskId', 404));
 			} else {
-				return res.jsonp(true, 'Deleted');
+				return res.jsonp(Utilities.response(true, task));
 			}
 		});
-};
+	},
+
+	getTaskByUserId: function(req, res) {
+		Tasks.find({
+			_userId: req.user._id.toString()
+		}).populate('_id').exec(function(err, tasks) {
+			if (err) {
+				return res.jsonp(Utilities.response(false, {}, 'Do not get tasks by userId'));
+			} else {
+				res.jsonp(Utilities.response(true, tasks));
+			}
+		});
+	},
+
+	createTask: function(req, res) {
+		console.log(req.body);
+		var task = new Tasks({
+			'_userId': req.user._id.toString(),
+			'isCompleted': req.body.isCompleted,
+			'date': req.body.date,
+			'content': req.body.content,
+			'estimation': req.body.estimation
+		});
+		console.log(task);
+		task.save(function(err, task) {
+			if (err) {
+				return res.jsonp(Utilities.response(false, {}, 'Do not save Task to Database', 404));
+			} else {
+				return res.jsonp(Utilities.response(true, task));
+			}
+		});
+	},
+
+	updateTask: function(req, res) {
+	},
+
+	deleteTask: function(req, res) {
+		Tasks.findByIdAndRemove(
+			req.user._id.toString(),
+			function(err) {
+				if (err) {
+					return res.jsonp(Utilities.response(false, {}, 'Do not delete Task ', 404));
+				} else {
+					return res.jsonp(true, 'Deleted');
+				}
+			});
+	},
+});
+
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var UsersController = require('../controllers/users');
+var ctrl = require('../controllers/users');
 var middleware = require('../config/middleware');
 
 /*GET*/
-router.get('/', function(req, res) {
-	res.jsonp('Daily-Scrum-api Users index');
-});
-router.get('/getUserById/:leanUserId', UsersController.getUserById);
-
+router.get('/getUserById/:leanUserId', ctrl.getUserById);
 /*POST*/
-router.post('/signup', UsersController.signup);
-router.post('/login', UsersController.login);
-router.post('/logout', UsersController.logout);
-
+router.post('/signup', ctrl.signup);
+router.post('/login', ctrl.login);
+router.post('/logout', ctrl.logout);
 /*PUT*/
-router.put('/changePassword/:userId', middleware.isAuthentication, UsersController.changePassword);
-
+router.put('/changePassword/:userId', middleware.isAuthentication, ctrl.changePassword);
 /*DELETE*/
-router.delete('/inactiveUserById/:userId', middleware.isAuthentication, UsersController.inactiveUserById);
-
+router.delete('/inactiveUserById/:userId', middleware.isAuthentication, ctrl.inactiveUserById);
 /*PARAM*/
-router.param('leanUserId', UsersController.queryLeanUser); //Lean
-router.param('userId', UsersController.queryUser);
+router.param('leanUserId', ctrl.queryLeanUser); //Lean
+router.param('userId', ctrl.queryUser);
+
+// function for set of collection
+router.get('/all', ctrl._getAll);
+router.get('/find', ctrl._find);
+router.get('/:_id/detail', ctrl._get);
+router.put('/:_id', ctrl._put);
 
 module.exports = router;
