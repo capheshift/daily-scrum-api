1f6c91a7da6a08f42060825602d727f52976ec64
Tam Pham <tampham47@live.com>
2015-11-17 00:02:46 +0700

    add more data for request project info

--- controllers/projects.js
+++ controllers/projects.js
@@ -4,12 +4,43 @@
 var _ = require('lodash');
 var mongoose = require('mongoose');
 var Projects = mongoose.model('Projects');
+var UserProject = mongoose.model('UserProject');
 var Utilities = require('../config/utilities');
 var Config = require('../config/config');
 var RestApi = require('../cores/restful');
 
+var async = require('async');
+var utils = require('../config/utilities');
+var debug = require('debug')('ctrl00');
+
 module.exports = exports = _.assign(RestApi(Projects), {
 	// more custom function will be here
+	_getAll: function(req, res) {
+		debug('_getAll 2');
+
+		Projects.find({}).populate(Projects.getPopulateFields()).exec().then(
+		function(projectList) {
+			var result = [];
+
+			async.each(projectList, function(item, callback) {
+				// debug('item', item);
+				var model = _.clone(item)._doc;
+				UserProject.find({_project: item._id}).populate('_user').exec()
+				.then(function(nData) {
+					model.members = nData;
+					result.push(model);
+					debug('DONE', model.name, model.members.length);
+					callback();
+				});
+			}, function(err) {
+				res.jsonp(utils.response(true, result));
+			});
+		},
+		function(err) {
+			res.jsonp(utils.response(false, null, err));
+		});
+	},
+
 	/**
 	 * getAll: function() {
 	 *   return {};
